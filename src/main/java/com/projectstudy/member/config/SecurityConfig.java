e216af4751043c655da691af13e77f08bc7ca2bf
jju00n <xord7@naver.com>
2023-01-14 01:07:35 +0900

    SecurityConfig passwordEncoder 적용

--- src/main/java/com/projectstudy/member/config/SecurityConfig.java
+++ src/main/java/com/projectstudy/member/config/SecurityConfig.java
@@ -4,8 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -13,6 +14,11 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
--- src/main/java/com/projectstudy/member/service/MemberService.java
+++ src/main/java/com/projectstudy/member/service/MemberService.java
@@ -4,6 +4,7 @@ import com.projectstudy.member.domain.Member;
 import com.projectstudy.member.dto.MemberDto;
 import com.projectstudy.member.repository.MemberRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,10 +14,12 @@ public class MemberService {
 
     public MemberDto saveMember(MemberDto memberDto) {
 
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
         Member member = Member.builder()
                 .userName(memberDto.getUserName())
                 .userId(memberDto.getUserId())
-                .userPw(memberDto.getUserPw())
+                .userPw(passwordEncoder.encode(memberDto.getUserPw()))
         .build();
 
         memberRepository.save(member);
